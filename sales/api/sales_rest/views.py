4b9331935336f0434a76d25d69dceeb9c19e7b13
Kristen <kristenlungstrum@yahoo.com>
2023-04-25 11:36:52 -0700

    finished backend

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -78,9 +78,11 @@ def api_customers(request):
                 encoder=CustomerEncoder,
                 safe=False,
             )
-        except Customer.DoesNotExist:
-            response = JsonResponse({"message": "Does not exist"})
-            response.status_code = 404
+        except:
+            response = JsonResponse(
+                {"message": "Could not create the customer"}
+            )
+            response.status_code = 400
             return response
 
 @require_http_methods(["GET", "PUT", "DELETE"])
@@ -108,6 +110,104 @@ def api_customer(request, id):
             return JsonResponse(
                 customer,
                 encoder=CustomerEncoder,
+                safe=False,
+            )
+        except Customer.DoesNotExist:
+            response = JsonResponse({"message": "Does not exist"})
+            response.status_code = 404
+            return response
+
+@require_http_methods(["GET", "POST"])
+def api_sales(request):
+    if request.method == "GET":
+        sales = Sale.objects.all()
+        return JsonResponse(
+            {"sales": sales},
+            encoder=SaleEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            automobile = AutomobileVO.objects.get(vin=content["automobile"])
+            content["automobile"] = automobile
+        except AutomobileVO.DoesNotExist:
+            response = JsonResponse({"message": "VIN does not exist"})
+            response.status_code = 404
+            return response
+        try:
+            salesperson = Salesperson.objects.get(id=content["salesperson"])
+            content["salesperson"] = salesperson
+        except Salesperson.DoesNotExist:
+            response = JsonResponse({"message": "Salesperson does not exist"})
+            response.status_code = 404
+            return response
+        try:
+            customer = Customer.objects.get(id=content["customer"])
+            content["customer"] = customer
+        except Customer.DoesNotExist:
+            response = JsonResponse({"message": "Customer does not exist"})
+            response.status_code = 404
+            return response
+        try:
+            sale = Sale.objects.create(**content)
+            return JsonResponse(
+                sale,
+                encoder=SaleEncoder,
+                safe=False,
+            )
+        except:
+            response = JsonResponse(
+                {"message": "Could not create the sale"}
+            )
+            response.status_code = 400
+            return response
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def api_sale(request, id):
+    if request.method == "GET":
+        try:
+            sale = Sale.objects.get(id=id)
+            return JsonResponse(
+                sale,
+                encoder=SaleEncoder,
+                safe=False,
+            )
+        except Sale.DoesNotExist:
+            response = JsonResponse({"message": "Does not exist"})
+            response.status_code = 404
+            return response
+    elif request.method == "DELETE":
+        count, _ = Sale.objects.filter(id=id).delete()
+        return JsonResponse({"deleted": count > 0})
+    else:
+        content = json.loads(request.body)
+        try:
+            automobile = AutomobileVO.objects.get(vin=content["automobile"])
+            content["automobile"] = automobile
+        except AutomobileVO.DoesNotExist:
+            response = JsonResponse({"message": "VIN does not exist"})
+            response.status_code = 404
+            return response
+        try:
+            salesperson = Salesperson.objects.get(id=content["salesperson"])
+            content["salesperson"] = salesperson
+        except Salesperson.DoesNotExist:
+            response = JsonResponse({"message": "Salesperson does not exist"})
+            response.status_code = 404
+            return response
+        try:
+            customer = Customer.objects.get(id=content["customer"])
+            content["customer"] = customer
+        except Customer.DoesNotExist:
+            response = JsonResponse({"message": "Customer does not exist"})
+            response.status_code = 404
+            return response
+        try:
+            Sale.objects.filter(id=id).update(**content)
+            sale = Sale.objects.get(id=id)
+            return JsonResponse(
+                sale,
+                encoder=SaleEncoder,
                 safe=False,
             )
         except Customer.DoesNotExist:
--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -14,13 +14,16 @@ django.setup()
 from sales_rest.models import AutomobileVO
 
 def get_automobiles():
-    response = requests.get("http://inventory-api:8000/api/automobiles/")
+    response = requests.get("http://project-beta-inventory-api-1:8000/api/automobiles/")
     content = json.loads(response.content)
     for automobile in content["autos"]:
         AutomobileVO.objects.update_or_create(
             import_href = automobile["href"],
             defaults={
+                "color": automobile["color"],
+                "year": automobile["year"],
                 "vin": automobile["vin"],
+                "sold": automobile["sold"],
             },
         )
 
