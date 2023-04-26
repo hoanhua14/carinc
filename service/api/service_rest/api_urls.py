6f0f31ccf59adab9cbd794ab0c0aebfa1e4c8a8e
So Hoan Hua <huasohoan@gmail.com>
2023-04-25 21:45:05 -0700

    Reverted back to old commit before duplicated files were synced and fixed code to the most recent working version

---
+++ service/api/service_rest/api_urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import api_list_techs, api_delete_tech, api_list_appointments, api_update_appointment, api_cancel_app, api_finish_app
+
+urlpatterns = [
+    path("technicians/", api_list_techs, name="api_list_techs"),
+    path("technicians/new/", api_list_techs, name="add_a_technician"),
+    path("technicians/<int:pk>/", api_delete_tech, name="delete_a_tech"),
+    path("appointments/", api_list_appointments, name="api_list_appointments"),
+    path("appointments/new/", api_list_appointments, name="create_an_appointment"),
+    path("appointments/delete/<int:pk>/", api_update_appointment, name="delete_an_app"),
+    path("appointments/<int:pk>/cancel", api_cancel_app, name="cancel_an_app"),
+    path("appointments/<int:pk>/finish", api_finish_app, name="finished_app"),
+]
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Technician(models.Model):
+    first_name = models.CharField(max_length=2000)
+    last_name = models.CharField(max_length=2000)
+    employee_id = models.CharField(max_length=2000)
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=2000)
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=2000)
+    status = models.CharField(max_length=2000)
+    vin = models.CharField(max_length=2000)
+    customer = models.CharField(max_length=2000)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointments",
+        on_delete=models.CASCADE,
+
+    )
